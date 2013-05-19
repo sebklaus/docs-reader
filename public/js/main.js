ae819aac5e1ec0cade99d9946de349f5f651bb53
Sebastian Klaus <sebastianklaus@icloud.com>
2013-05-19 22:20:34 +0800

    Fixed target="_blank" for https:// links
    Signed-off-by: Sebastian Klaus <sebastianklaus@icloud.com>

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,7 @@ jQuery(document).ready(function($)
     // Remove <strong> tag and add nav-header class to its parent
     $('aside.sidebar li strong').contents().unwrap().parent().addClass('nav-header').prepend('<i class="icon-folder-open"></i>');
     // Open all external links in a new window/ tab
-    $('a[href^="http://"]').attr({target:"_blank", title:"Opens in a new window"});
+    $('a[href^="http://"], a[href^="https://"]').attr({target:"_blank", title:"Opens in a new window"});
     // Make tables look nice
     $('.content table').addClass('table table-striped table-condensed table-bordered');
 });
