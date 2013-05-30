cb79e2f7b5032e83bc2cf2c9c6c5e380f372891f
Sebastian Klaus <sebastian.klaus@aegiscare.com.au>
2013-05-30 14:12:49 +0800

    Moved more styling to LESS/CSS, away from JS
    Signed-off-by: Sebastian Klaus <sebastian.klaus@aegiscare.com.au>

--- public/js/main.js
+++ public/js/main.js
@@ -9,13 +9,10 @@ jQuery(document).ready(function($)
     prettyPrint();
     // Add Twitter Bootstrap related classes to HTML tags
     $('aside.sidebar ul').addClass('nav nav-list');
-    $('.content blockquote').addClass('alert alert-info');
     // Remove <em> tag  and add divider class to its parent
     $('aside.sidebar li em').contents().unwrap().parent().addClass('divider');
     // Remove <strong> tag and add nav-header class to its parent
     $('aside.sidebar li strong').contents().unwrap().parent().addClass('nav-header').prepend('<i class="icon-folder-open"></i>');
     // Open all external links in a new window/ tab
     $('a[href^="http://"], a[href^="https://"]').attr({target:"_blank", title:"Opens in a new window"});
-    // Make tables look nice
-    $('.content table').addClass('table table-striped table-condensed table-bordered');
 });
